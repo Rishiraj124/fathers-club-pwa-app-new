44be50d957bdd02f2102c014b297ace8828e6b05
RishiDevGit <rishirajsaxena@makewebbetter.com>
2024-03-14 23:54:44 +0530

    Adding tabler package changes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,19 @@
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <ol className='breadcrumb' aria-label='breadcrumbs'>
+        <li className='breadcrumb-item'>
+          <a href='#'>Home</a>
+        </li>
+        <li className='breadcrumb-item'>
+          <a href='#'>Library</a>
+        </li>
+        <li className='breadcrumb-item active' aria-current='page'>
+          <a href='#'>Data</a>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 export default page;
