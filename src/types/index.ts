98e28f1e0be40ce611d53e5bfb745bd62e2c99b1
RishiDevGit <rishirajsaxena@makewebbetter.com>
2024-03-14 14:14:45 +0530

    Type variables creation changes

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+// ====== EVENT PARAMS
+export type CreateEventParams = {
+  userId: string;
+  event: {
+    title: string;
+    description: string;
+    location: string;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    categoryId: string;
+    price: string;
+    isFree: boolean;
+    url: string;
+  };
+  path: string;
+};
+// ====== USER PARAMS
+export type CreateUserParams = {
+  clerkId: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  photo: string;
+};
